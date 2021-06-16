f52581d561fd19005222812570c08204aba24325
siphomsiza <siphosmall@gmail.com>
2021-06-16 10:27:56 +0200

    fixing validation of option must be uniq

--- app/models/survey/question.rb
+++ app/models/survey/question.rb
@@ -5,16 +5,16 @@ class Survey::Question < ActiveRecord::Base
   has_many   :options
   has_many   :predefined_values
   belongs_to :section
-  
+
   #rails 3 attr_accessible support
   if Rails::VERSION::MAJOR < 4
     attr_accessible :options_attributes, :predefined_values_attributes, :text, :section_id, :head_number, :description, :locale_text, :locale_head_number, :locale_description, :questions_type_id
   end
-  
+
   accepts_nested_attributes_for :options,
     :reject_if => ->(a) { a[:options_type_id].blank? },
       :allow_destroy => true
-  
+
   accepts_nested_attributes_for :predefined_values,
     :reject_if => ->(a) { a[:name].blank? },
       :allow_destroy => true
@@ -23,9 +23,10 @@ class Survey::Question < ActiveRecord::Base
   validates :text, :presence => true, :allow_blank => false
   validates :questions_type_id, :presence => true
   validates_inclusion_of :questions_type_id, :in => Survey::QuestionsType.questions_type_ids, :unless => Proc.new{|q| q.questions_type_id.blank?}
-  
+  validate :uniqueness_of_options
+
   scope :mandatory_only, -> { where(:mandatory => true) }
-  
+
   def correct_options
     options.correct
   end
@@ -33,19 +34,23 @@ class Survey::Question < ActiveRecord::Base
   def incorrect_options
     options.incorrect
   end
-  
+
   def text
     I18n.locale == I18n.default_locale ? super : locale_text.blank? ? super : locale_text
   end
-  
+
   def description
     I18n.locale == I18n.default_locale ? super : locale_description.blank? ? super : locale_description
   end
-  
+
   def head_number
     I18n.locale == I18n.default_locale ? super : locale_head_number.blank? ? super : locale_head_number
   end
-  
+
+  def uniqueness_of_options
+    errors.add(:options, 'must be unique') if options.map(&:text).uniq.size != options.size
+  end
+
   def mandatory?
     self.mandatory == true
   end
