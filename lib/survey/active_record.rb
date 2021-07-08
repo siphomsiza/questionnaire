6aaf47749c3a02ef01d083d2620f7ce6ad321c05
siphomsiza <siphosmall@gmail.com>
2021-07-08 19:51:21 +0200

    Update active_record.rb

--- lib/survey/active_record.rb
+++ lib/survey/active_record.rb
@@ -4,7 +4,7 @@ module Survey
 
     module ClassMethods
       def has_surveys
-        has_many :survey_attempts, as: :participant, :class_name => ::Survey::Attempt
+        has_many :survey_attempts, as: :participant, :class_name => "::Survey::Attempt"
 
         define_method("for_survey") do |survey|
           self.survey_attempts.where(:survey_id => survey.id)
